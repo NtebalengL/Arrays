e630e0ec04f796fbdf5fdb329aafef2f51b074bc
Dell-User <Dell-User@DESKTOP-I2HH80B>
2023-10-10 08:21:05 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+
+int[] LuckyNumbers = { 4, 5, 8, 9 };
+Console.WriteLine(LuckyNumbers[3]);
+
+//Stray Population
+String[] Friends = new string[6];
+Friends[0] = "Danny";
+Friends[1] ="Robin";
+Friends[2] ="Raven";
+Friends[3] ="Garfield";
+Friends[4] ="Miranda";
+Friends[5] ="Tiffany";
+Console.WriteLine(Friends[4]);
+
+
